ed3cccbd03495a1c66dd691f431c2d90c77b1007
Vincent Frammery <vincent.frammery@cgos.asso.fr>
2023-12-24 18:52:18 +0100

    Update with Section option in source target file

--- getput.py
+++ getput.py
@@ -1,42 +1,34 @@
+import configparser
 import re
 
-def read_ini_field(file_path, field):
-    with open(file_path, 'r') as file:
-        for line in file:
-            if line.startswith(field):
-                return line.split('=')[1].strip()
-    return None
-
-def write_ini_field(file_path, field, value):
-    lines = []
-    found = False
-    with open(file_path, 'r') as file:
-        for line in file:
-            if line.startswith(field):
-                lines.append(f'{field}={value}\n')
-                found = True
-            else:
-                lines.append(line)
-
-    if not found:
-        lines.append(f'{field}={value}\n')
-
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
-
 def process_instruction(line):
-    pattern = r"file_source:'(.+)' field_source:'(.+)' file_target:'(.+)' field_target:'(.+)'"
+    # Expression régulière pour extraire les informations avec des sections optionnelles
+    pattern = r"file_source:'(.+)'(?: section_source:'(.+)')? field_source:'(.+)' file_target:'(.+)'(?: section_target:'(.+)')? field_target:'(.+)'"
     match = re.match(pattern, line)
 
     if match:
-        file_source, field_source, file_target, field_target = match.groups()
+        file_source, section_source, field_source, file_target, section_target, field_target = match.groups()
+
+        # Utiliser la section 'DEFAULT' si aucune section n'est spécifiée
+        if not section_source:
+            section_source = 'DEFAULT'
+        if not section_target:
+            section_target = 'DEFAULT'
 
         # Lire la valeur du champ source
-        value = read_ini_field(file_source, field_source)
+        config_source = configparser.ConfigParser()
+        config_source.read(file_source)
+        value = config_source.get(section_source, field_source, fallback=None)
 
         # Écrire la valeur dans le champ cible
         if value is not None:
-            write_ini_field(file_target, field_target, value)
+            config_target = configparser.ConfigParser()
+            config_target.read(file_target)
+            if not config_target.has_section(section_target) and section_target != 'DEFAULT':
+                config_target.add_section(section_target)
+            config_target.set(section_target, field_target, value)
+            with open(file_target, 'w') as target_file:
+                config_target.write(target_file)
 
 def execute_instructions_from_file(file_path):
     with open(file_path, 'r') as file:
