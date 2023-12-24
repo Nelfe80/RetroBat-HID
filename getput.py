bad55b84312e4ff9c2144eaa49c79468fc5cd850
Vincent Frammery <vincent.frammery@cgos.asso.fr>
2023-12-24 19:07:31 +0100

    Update with Section with if condition

--- getput.py
+++ getput.py
@@ -1,34 +1,35 @@
 import configparser
 import re
 
+def condition_met(config, section, condition):
+    if condition:
+        key, value = [x.strip() for x in condition.split('=')]
+        return config.get(section, key, fallback=None) == value
+    return True
+
 def process_instruction(line):
-    # Expression régulière pour extraire les informations avec des sections optionnelles
-    pattern = r"file_source:'(.+)'(?: section_source:'(.+)')? field_source:'(.+)' file_target:'(.+)'(?: section_target:'(.+)')? field_target:'(.+)'"
+    pattern = r"file_source:'(.+)' section_source:'(.+)' field_source:'(.+)'(?: if_source:'(.+)')? file_target:'(.+)' section_target:'(.+)' field_target:'(.+)'"
     match = re.match(pattern, line)
 
     if match:
-        file_source, section_source, field_source, file_target, section_target, field_target = match.groups()
+        file_source, section_source, field_source, if_source, file_target, section_target, field_target = match.groups()
 
-        # Utiliser la section 'DEFAULT' si aucune section n'est spécifiée
-        if not section_source:
-            section_source = 'DEFAULT'
-        if not section_target:
-            section_target = 'DEFAULT'
-
-        # Lire la valeur du champ source
+        # Lire la source
         config_source = configparser.ConfigParser()
         config_source.read(file_source)
-        value = config_source.get(section_source, field_source, fallback=None)
 
-        # Écrire la valeur dans le champ cible
-        if value is not None:
-            config_target = configparser.ConfigParser()
-            config_target.read(file_target)
-            if not config_target.has_section(section_target) and section_target != 'DEFAULT':
-                config_target.add_section(section_target)
-            config_target.set(section_target, field_target, value)
-            with open(file_target, 'w') as target_file:
-                config_target.write(target_file)
+        if condition_met(config_source, section_source, if_source):
+            value = config_source.get(section_source, field_source, fallback=None)
+
+            # Écrire dans la cible
+            if value is not None:
+                config_target = configparser.ConfigParser()
+                config_target.read(file_target)
+                if not config_target.has_section(section_target):
+                    config_target.add_section(section_target)
+                config_target.set(section_target, field_target, value)
+                with open(file_target, 'w') as target_file:
+                    config_target.write(target_file)
 
 def execute_instructions_from_file(file_path):
     with open(file_path, 'r') as file:
